92337b7f19d9d111a9ad0a4379a240d8fa19c904
Nicole <nicolebirchh@gmail.com>
2026-07-28 12:17:32 +1000

    pushing files to git

---
+++ 1P/light_levels/light_levels.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightSensor;
+
+// Time between readings in milliseconds.
+const unsigned long SAMPLE_INTERVAL_MS = 10000;
+
+// Stores the millis() value from the last reading so the loop can time
+// itself without using the blocking delay() function 
+unsigned long lastSampleTime = 0;
+
+void setup() {
+  // Starts the serial connection on baud 9600
+  Serial.begin(9600);
+  while (!Serial); // blocks the port until the python script opens it
+
+  // Start the I2C bus
+  Wire.begin();
+
+  // Attempt to initialise the light sensor. Program will not begin if there is an error connecting
+  // Serial prints with # will be ignored in the python script
+  while(!lightSensor.begin()) {
+    Serial.println("# ERROR: BH1750 not found. Check sensor connections");
+    delay(1000);
+  }
+
+  Serial.println("# BH1750 connected");
+}
+
+void loop() {
+  // set now to current uptime of the board to assist in tracking the next sample
+  unsigned long now = millis(); 
+
+  // Gates the light reading to only sample the set interval
+  if (now - lastSampleTime >= SAMPLE_INTERVAL_MS) {
+    lastSampleTime = now; // Reset the sample timer
+
+    float lux = lightSensor.readLightLevel();
+
+    // If a reading is outside the bounds of the sensor or a large light spike
+    // which will be marked as bad readings. Otherwise log the lux to serial 
+    if (lux < 0 || lux > 1500) {
+      Serial.println("# READ_ERROR");
+    } else {
+      Serial.println(lux);
+    }
+  }
+}
